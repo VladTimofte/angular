b32faa64759eed65ddd38a93c38e1a269b92d8d3
Vladimir Timofte <Vladimir.Timofte@partner.bmwgroup.com>
2024-06-18 11:15:07 +0300

    Directives project modified

---
+++ directives/src/app/safe-link.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, input } from '@angular/core'
+
+@Directive({
+    selector: "a[appSafeLink]",
+    standalone: true,
+    host: {
+        '(click)': 'onConfirmLeavePage($event)'
+    }
+})
+export class SafeLinkDirective {
+
+    queryParam = input('myApp')
+
+constructor() {
+    console.log('SafeLinkDirective Loaded')
+}
+
+    onConfirmLeavePage(event: MouseEvent) {
+        const wantsToLeave = window.confirm('Do you want to leave the app?');
+
+        if (wantsToLeave) {
+            const address = (event.target as HTMLAnchorElement).href;
+            (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+            return;
+        }
+
+        event?.preventDefault();
+    }   
+
+}
